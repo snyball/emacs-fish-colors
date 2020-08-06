e12b1072e40c21fbb8fc0d3a596ad2d5239210de
Jonas Møller <jonas.moller@student.uib.no>
2020-08-06 17:08:16 +0200

    Added appropriate metadata.

--- fish-colors.el
+++ fish-colors.el
@@ -1,4 +1,41 @@
-;;; ~/.config/doom/ml/fish-colors.el -*- lexical-binding: t; -*-
+;;; fish-colors.el --- Export Emacs color theme to fish. -*- lexical-binding: t -*-
+;;; Commentary:
+;;;
+;;; Run M-x fish-colors-install-function and add the following to your
+;;; fish config.el file:
+;;;
+;;;   if set -q INSIDE_EMACS
+;;;       set_emacs_colors
+;;;   end
+;;;
+;;; Author: Jonas Møller <jonas.moeller2@protonmail.com>
+;;; URL: https://github.com/snyball/emacs-fish-colors
+;;; Version: 0.1
+;;; Keywords: color theme fish shell
+;;; License:
+;;;   Copyright (C) 2020 Jonas Møller
+;;;
+;;;   Redistribution and use in source and binary forms, with or without
+;;;   modification, are permitted provided that the following conditions are met:
+;;;
+;;;   1. Redistributions of source code must retain the above copyright notice, this
+;;;      list of conditions and the following disclaimer.
+;;;
+;;;   2. Redistributions in binary form must reproduce the above copyright notice,
+;;;      this list of conditions and the following disclaimer in the documentation
+;;;      and/or other materials provided with the distribution.
+;;;
+;;;   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+;;;   ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+;;;   WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+;;;   DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+;;;   ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+;;;   (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+;;;   LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+;;;   ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+;;;   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+;;;   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+;;; Code:
 
 (require 'xdg)
 (require 's)
@@ -30,7 +67,7 @@
 
 ;;;###autoload
 (defun fish-colors--face-to-fish-color (face)
-  "Convert an Emacs face to set_color syntax."
+  "Convert an Emacs FACE to set_color syntax."
   (let ((color (face-attribute face :foreground))
         (default-color (face-attribute 'default :foreground)))
     (s-join " " `(,(format "'%s'" (if (eq color :unspecified)
@@ -47,7 +84,7 @@
 
 ;;;###autoload
 (defun fish-colors--make-commands (&optional indent)
-  "Make the commands to set fish colors."
+  "Make the commands to set fish colors, optionally indented with INDENT spaces."
   (setq indent (if indent
                    (s-repeat indent " ")
                  ""))
@@ -84,3 +121,7 @@
       (insert (fish-colors--make-function))
       (save-buffer)
       (kill-buffer))))
+
+(provide 'fish-colors)
+
+;;; fish-colors.el ends here
